bdee121897b01785d6a8686e8ec611b84ec8575f
TAE YOUNG KIM <ty2kim@gmail.com>
2016-10-19 17:17:11 +0000

    validation done

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,66 +4,23 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from tweets.json
-var data =
-[
-  {
-    user: {
-      name: 'Newton',
-      avatars: {
-        small:   'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png',
-        regular: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png',
-        large:   'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png',
-      },
-      handle: '@SirIsaac',
-    },
-    content: {
-      text: 'If I have seen further it is by standing on the shoulders of giants',
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: {
-        small:   'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png',
-        regular: 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png',
-        large:   'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png',
-      },
-      handle: '@rd',
-    },
-    content: {
-      text: 'Je pense , donc je suis',
-    },
-    created_at: 1461113959088,
-  },
-  {
-    user: {
-      name: 'Johann von Goethe',
-      avatars: {
-        small:   'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png',
-        regular: 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png',
-        large:   'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png',
-      },
-      handle: '@johann49',
-    },
-    content: {
-      text: 'Es ist nichts schrecklicher als eine tätige Unwissenheit.',
-    },
-    created_at: 1461113796368,
-  },
-];
-
 $(document).ready(function () {
   $('.new-tweet').on('submit', 'form', function (event) {
     event.preventDefault();
+    var counter = +$(this).find('.counter').text();
     var $text = $(this).find('textarea');
-    var queryStr = $text.serialize();
-    $.ajax({
-      url: '/tweets',
-      method: 'POST',
-      data: queryStr,
-    });
+    if (counter < 0) {
+      alert('maximum tweet is 140');
+    } else if (!$text.text()) {
+      alert('you didn\'t write anything');
+    } else {
+      var queryStr = $text.serialize();
+      $.ajax({
+        url: '/tweets',
+        method: 'POST',
+        data: queryStr,
+      });
+    }
   });
 
   function renderTweets(tweets) {
@@ -99,5 +56,15 @@ $(document).ready(function () {
     return $tweet;
   }
 
-  renderTweets(data);
+  function loadTweets() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function (tweets) {
+        renderTweets(tweets);
+      },
+    });
+  }
+
+  loadTweets();
 });
