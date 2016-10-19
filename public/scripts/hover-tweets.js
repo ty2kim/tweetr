0b68550a914d3ea455969db185ae8b6c53131467
TAE YOUNG KIM <ty2kim@gmail.com>
2016-10-19 03:53:48 +0000

    dynamic tweets done

--- public/scripts/hover-tweets.js
+++ public/scripts/hover-tweets.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   $('.tweet').hover(function (event) {
     $(this).find('.social').toggle();
+  }).mouseenter(function (event) {
+    $(this).addClass('hover');
+  }).mouseleave(function (event) {
+    $(this).removeClass('hover');
   });
 });
