f309c62d71771735bd4b262976ba865579202fcb
TAE YOUNG KIM <ty2kim@gmail.com>
2016-10-21 23:30:49 +0000

    liking tweets without mongo

---
+++ server/lib/tweet-helper.js
@@ -0,0 +1,12 @@
+module.exports = {
+  generateRandomString: () => {
+    const length = 6;
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (var i = length; i > 0; --i) {
+      result += chars[Math.floor(Math.random() * chars.length)];
+    }
+
+    return result;
+  },
+};
